0ecda4c2c09798bba97e4e9ee9d7a14efcce2d65
raymond-chii <raymondchi56@gmail.com>
2024-02-24 17:08:23 -0500

    update

--- database/articles.sql
+++ database/articles.sql
@@ -1,5 +1,6 @@
 CREATE SEQUENCE article_id_seq START WITH 1;
 CREATE TYPE article_status AS ENUM('pending', 'approved', 'rejected');
+CREATE TYPE category AS ENUM('News', 'Opinion', 'Events', 'Letters', 'Face of Cooper', 'Alumni Interview', 'Comics','Interviews','Columns', 'Sports');
 
 CREATE TABLE articles(
     article_id bigint not null default nextval('article_id_seq'),
@@ -10,6 +11,7 @@ CREATE TABLE articles(
     article_status article_status not null,
     picture varchar(255),
     media_url varchar(255),
+    category category not null,
 
     PRIMARY KEY (article_id),
     FOREIGN KEY (user_id) REFERENCES users(user_id)
